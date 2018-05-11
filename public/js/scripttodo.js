8da08efbd6b983bef6cdfb33bebb82318a5686bc
Aksha10 <akshata.shivade@raweng.com>
2018-05-11 19:08:58 +0530

    todo list

--- public/js/scripttodo.js
+++ public/js/scripttodo.js
@@ -2,25 +2,52 @@
 function newListItem() {
 	var li = document.createElement("li");
 	var inputValue = document.getElementById("inputtxt").value;
-	var t = document.createTextNode(inputValue);
-	li.innerHTML = '<input class="checkbox" type="checkbox"> <button class="close" type="button">&times;</button>';
-	li.appendChild(t); 
-	if (inputValue === '') {
-		document.getElementById("warning").innerHTML = "Warning:Please enter valid input value";
-	} 
-	else {
-		document.getElementById("warning").innerHTML="";
-		document.getElementById("ulItem").appendChild(li);
-	}
-	document.getElementById("inputtxt").value = "";
+	var data = { data: inputValue };
+	console.log(inputValue);
+	
+	$.ajax({
+		type: 'POST',
+		data: JSON.stringify(data),
+		contentType: 'application/json',
+		url: 'http://localhost:3001/addNewTask',
+		success: function (data) {
+			console.log('success');
+			console.log(JSON.stringify(data));
+			var t = document.createTextNode(inputValue);
+			li.innerHTML = '<input class="checkbox" type="checkbox"/><span class="input">'+inputValue+'</span><button class="close" type="button">&times;</button>';
+			
+			console.log(li);
+			if (inputValue === '') {
+				document.getElementById("warning").innerHTML = "Warning:Please enter valid input value";
+			}
+			else {
+				document.getElementById("warning").innerHTML = "";
+				document.getElementById("ulItem").appendChild(li);
+			}
+			document.getElementById("inputtxt").value = "";
+		}
+	});
 }
 //for close button
-$(document).ready( function(){
-	$(document).on('click','.close',function(){	
-		$(this).closest("li").remove();
+$(document).ready(function () {
+	$(document).on('click', '.close', function () {
+		var removeTxt = $(this).closest("li").find('.input').text();
+		$.ajax({
+			url: '/delete?todo='+removeTxt,
+			type: 'delete',
+			success: function(result) {	
+				$(this).closest("li").remove();
+				
+			}
+		});	
+		console.log(removeTxt);	
 	});
-  //mark all
-  $(".markallBtn").click(function() {
+	//active
+	// $(".activeBtn").click(function() {
+
+	// })
+	//mark all
+	$(".markallBtn").click(function () {
 		if ($("li").hasClass("liMarkAll")) {
 			$("li").removeClass("liMarkAll");
 			$('.checkbox').removeAttr('checked');
@@ -31,7 +58,7 @@ $(document).ready( function(){
 		}
 	});
 	//remove mark when click on checkbox
-	$(document).on('change','.checkbox', function() {
+	$(document).on('change', '.checkbox', function () {
 		if ($(this).is(':checked')) {
 			$(this).parent().addClass("liMarkAll");
 		}
